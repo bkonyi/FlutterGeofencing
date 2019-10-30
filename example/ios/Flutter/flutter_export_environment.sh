c3972a2014357a049c8f074fadb33215272d97d4
Kresten P. Vester <kresten.vester@schneider-electric.com>
2019-10-30 09:04:51 +0100

    Reverting more changes to improve pull request

--- example/ios/Flutter/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/kvester/opt/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/kvester/work/FlutterGeofencing/example"
-export "FLUTTER_TARGET=lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/kvester/opt/flutter/bin/cache/artifacts/engine/ios"
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,7 +7,7 @@ import 'dart:isolate';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:geofencing/flutter_geofencing.dart';
+import 'package:geofencing/geofencing.dart';
 
 void main() => runApp(MyApp());
 
