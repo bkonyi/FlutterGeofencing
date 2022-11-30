716de6613d23d764f7bbfc0ae150c73648b1ab33
vovaklh <vovaklh20012@gmail.com>
2022-11-30 20:34:09 +0200

    remove pragma

--- lib/src/callback_dispatcher.dart
+++ lib/src/callback_dispatcher.dart
@@ -10,7 +10,6 @@ import 'package:flutter/services.dart';
 import 'package:geofencing/src/geofencing.dart';
 import 'package:geofencing/src/location.dart';
 
-@pragma('vm:entry-point')
 void callbackDispatcher() {
   const MethodChannel _backgroundChannel =
       MethodChannel('plugins.flutter.io/geofencing_plugin_background');
